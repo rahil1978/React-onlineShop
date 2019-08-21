40469a124cdbeecfb16db9d1cf01c72167ac0e2d
Raheel Aslam <rahil178@gmail.com>
2019-08-21 23:02:36 +1200

    Added collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
+
 
 function App() {
-  return (
-    <div> 
-    <HomePage/>
+  return (     
+    <div>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/Shop' component={ShopPage}/>
     </div>
   );
 }
