509798dc1dc625b9e77628106334149c721e663a
Raheel Aslam <rahil178@gmail.com>
2019-08-26 15:10:31 +1200

    Implemented the signup page with firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
     appId: "1:710374212555:web:4224fb3cb4731775"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionData) => {
+    if(!userAuth) return; 
+    const userRef = firestore.doc(`users/${userAuth.uid}`); 
+    const snapShot = await userRef.get(); 
+    
+    if(!snapShot.exists){
+
+      const {displayName,email} = userAuth; 
+      const createdAt = new Date(); 
+
+      
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionData
+        })
+      } catch(error){
+        console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef; 
+  };
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
